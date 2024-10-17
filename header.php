64c2a72377e19de40f7602998175f79068452ebf
Divyesh880 <divyesh@appletechconsultants.com>
2024-10-17 14:01:24 +0530

    Add files via upload

--- header.php
+++ header.php
@@ -1,11 +1,12 @@
 <div class="top_header">
     <div>
-        <span class="sidebar_toggle_btn" onclick="toggleNav()">&#9776;</span> 
+        <span class="sidebar_toggle_btn" onclick="toggleNav()">&#9776;</span>
     </div>
     <div class="dropdown_box">
         <span>John Thompson</span>
-        <img src="assets/images/Profile_top_orange.png" alt="Profile" class="dropdown_image" onclick="toggleDropdown()">
-        <div class="d-inline-flex"><a href="login.php" class="login_btn">Login</a></div>
+        <img src="assets/images/header/profile_orange.png" alt="Profile" class="dropdown_image"
+            onclick="toggleDropdown()">
+        <div class="d-inline-flex"><a href="login.html" class="login_btn">Login</a></div>
         <div class="dropdown_content" id="profile_dropdown">
             <a href="#"><i class="fa-solid fa-user"></i> &nbsp; Profile</a>
             <a href="#"><i class="fa-solid fa-gear"></i> &nbsp; Settings</a>
--- sidebar.php
+++ sidebar.php
@@ -1,31 +1,31 @@
 <div id="sidebar" class="sidenav_bar">
     <a href="index.html">
         <div class="site_logo">
-            <img src="assets/images/logo.png" alt="logo" title="logo">
+            <img src="assets/images/logo/logo.png" alt="logo" title="logo">
         </div>
     </a>
     <!-- <a href="index.html" class="sidebar_link">
-        <img src="assets/images/Dashboard.png" alt="Dashboard" width="28" class="sidebar_icons"> 
+        <img src="assets/images/sidebar/dashboard_orange.png" alt="Dashboard" width="28" class="sidebar_icons"> 
         <span class="sidemenu_text">Dashboard</span>
     </a> -->
     <a href="grant-success.html" class="sidebar_link">
-        <img src="assets/images/Ask_Question_orange.png" alt="Ask A Question" width="28" class="sidebar_icons">
+        <img src="assets/images/sidebar/grant_success_orange.png" alt="Grant Success" width="28" class="sidebar_icons">
         <span class="sidemenu_text">Grant Success: Ask Away!</span>
     </a>
     <!-- <a href="got-questions.html" class="sidebar_link">
-        <img src="assets/images/questions_orange.png" alt="Ask A Question" width="28" class="sidebar_icons">
+        <img src="assets/images/sidebar/got_questions_orange.png" alt="Got Questions" width="28" class="sidebar_icons">
         <span class="sidemenu_text">Got Questions? Let’s Ignite!</span>
     </a> -->
     <a href="batch-processing.html" class="sidebar_link">
-        <img src="assets/images/Batch_Processing_orange.png" alt="Batch Process" width="28" class="sidebar_icons">
+        <img src="assets/images/sidebar/batch_processing_orange.png" alt="Batch Process" width="28" class="sidebar_icons">
         <span class="sidemenu_text">Batch Processing</span>
     </a>
     <a href="download-logs.html" class="sidebar_link">
-        <img src="assets/images/Download_orange.png" alt="Download Logs" width="28" class="sidebar_icons">
+        <img src="assets/images/sidebar/download_logs_orange.png" alt="Download Logs" width="28" class="sidebar_icons">
         <span class="sidemenu_text">Download Logs</span>
     </a>
     <a href="future-vision.html" class="sidebar_link">
-        <img src="assets/images/future_vision_orange.png" alt="Future Vision" width="28" class="sidebar_icons">
+        <img src="assets/images/sidebar/future_vision_orange.png" alt="Future Vision" width="28" class="sidebar_icons">
         <span class="sidemenu_text">Future Vision</span>
     </a>
 </div>
