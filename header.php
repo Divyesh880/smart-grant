bca581d5031d5bd8fa0ba6c308f3cb712517b3b1
Divyesh880 <divyesh@appletechconsultants.com>
2024-10-30 14:34:35 +0530

    Add files via upload

--- header.php
+++ header.php
@@ -2,13 +2,11 @@
     <div>
         <span onclick="toggleNav()">
             <i onclick="iconChange(this)" class="sidebar_toggle_btn fa fa-bars"></i>
-            <!-- <i class="fa">&#xf0c9;</i> -->
         </span>
     </div>
     <div class="dropdown_box">
         <span>John Thompson</span>
-        <img src="assets/images/header/profile_orange.png" alt="Profile" class="dropdown_image"
-            onclick="toggleDropdown()">
+        <img src="assets/images/header/profile_orange.png" alt="Profile" class="dropdown_image" onclick="toggleDropdown()">
         <div class="d-inline-flex"><a href="login.html" class="login_btn">Login</a></div>
         <div class="dropdown_content" id="profile_dropdown">
             <a href="#"><i class="fa-solid fa-user"></i> &nbsp; Profile</a>
