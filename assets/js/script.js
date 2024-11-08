d57e475995a2b2883c1ac4e70e627444c61dae06
Divyesh880 <divyesh@appletechconsultants.com>
2024-11-08 18:38:21 +0530

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,57 +30,8 @@ window.onclick = function(event) {
 // Dropdown Open and Close Script End
 
 // Sidebar Open and Close for Big and Small Screen Script Start
-// let isNavOpen = window.innerWidth > 768; 
-// let dropdownVisible = false; 
-
-// function toggleNav() {
-//     if (isNavOpen) {
-//         closeNav();
-//     } else {
-//         openNav();
-//     }
-// }
-
-// function openNav() {
-//     document.getElementById("sidebar").style.width = "350px";
-//     document.getElementById("main_container").style.marginLeft = window.innerWidth <= 768 ? "0" : "350px";
-//     isNavOpen = true;
-//     document.getElementById("sidebar").classList.remove('collapsed');
-//     document.getElementById("sidebar").classList.add('open_collapsed');
-// }
-
-// function closeNav() {
-//     document.getElementById("sidebar").style.width = "0";
-//     document.getElementById("main_container").style.marginLeft = "0"; 
-//     isNavOpen = false;
-//     document.getElementById("sidebar").classList.add('collapsed');
-//     document.getElementById("sidebar").classList.remove('open_collapsed');
-// }
-
-// window.onload = function() {
-//     if (isNavOpen) {
-//         openNav();
-//     } else {
-//         closeNav();
-//     }
-// };
-
-// window.onresize = function() {
-//     if (window.innerWidth > 768 && !isNavOpen) {
-//         openNav();
-//     } else if (window.innerWidth <= 768 && isNavOpen) {
-//         closeNav();
-//     }
-// };
-
-// document.getElementById('toggleButton').addEventListener('click', toggleNav);
-// Sidebar Open and Close for Big and Small Screen Script End
-
-// Sidebar Collapsed(Close) by Default for Big and Small Screen Script Start
-let isNavOpen = false;
-
-document.getElementById("sidebar").classList.add('collapsed');
-document.getElementById("main_container").style.marginLeft = "0";
+let isNavOpen = window.innerWidth > 768; 
+let dropdownVisible = false; 
 
 function toggleNav() {
     if (isNavOpen) {
@@ -92,7 +43,7 @@ function toggleNav() {
 
 function openNav() {
     document.getElementById("sidebar").style.width = "350px";
-    document.getElementById("main_container").style.marginLeft = "350px";
+    document.getElementById("main_container").style.marginLeft = window.innerWidth <= 768 ? "0" : "350px";
     isNavOpen = true;
     document.getElementById("sidebar").classList.remove('collapsed');
     document.getElementById("sidebar").classList.add('open_collapsed');
@@ -100,14 +51,62 @@ function openNav() {
 
 function closeNav() {
     document.getElementById("sidebar").style.width = "0";
-    document.getElementById("main_container").style.marginLeft = "0";
+    document.getElementById("main_container").style.marginLeft = "0"; 
     isNavOpen = false;
     document.getElementById("sidebar").classList.add('collapsed');
     document.getElementById("sidebar").classList.remove('open_collapsed');
 }
 
-document.getElementById('toggleButton').addEventListener('click', toggleNav);
+window.onload = function() {
+    if (isNavOpen) {
+        openNav();
+    } else {
+        closeNav();
+    }
+};
 
+window.onresize = function() {
+    if (window.innerWidth > 768 && !isNavOpen) {
+        openNav();
+    } else if (window.innerWidth <= 768 && isNavOpen) {
+        closeNav();
+    }
+};
+
+document.getElementById('toggleButton').addEventListener('click', toggleNav);
+// Sidebar Open and Close for Big and Small Screen Script End
+
+// Sidebar Collapsed(Close) by Default for Big and Small Screen Script Start
+// let isNavOpen = false;
+
+// document.getElementById("sidebar").classList.add('collapsed');
+// document.getElementById("main_container").style.marginLeft = "0";
+
+// function toggleNav() {
+//     if (isNavOpen) {
+//         closeNav();
+//     } else {
+//         openNav();
+//     }
+// }
+
+// function openNav() {
+//     document.getElementById("sidebar").style.width = "350px";
+//     document.getElementById("main_container").style.marginLeft = "350px";
+//     isNavOpen = true;
+//     document.getElementById("sidebar").classList.remove('collapsed');
+//     document.getElementById("sidebar").classList.add('open_collapsed');
+// }
+
+// function closeNav() {
+//     document.getElementById("sidebar").style.width = "0";
+//     document.getElementById("main_container").style.marginLeft = "0";
+//     isNavOpen = false;
+//     document.getElementById("sidebar").classList.add('collapsed');
+//     document.getElementById("sidebar").classList.remove('open_collapsed');
+// }
+
+// document.getElementById('toggleButton').addEventListener('click', toggleNav);
 // // Sidebar Collapsed(Close) by Default for Big and Small Screen Script End
 
 // Sidebar Toggle Icon Change on Click Script Start
